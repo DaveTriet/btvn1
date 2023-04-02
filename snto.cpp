ef55b0a083c2ff65c6139c752947965091666a1a
ANormalProgrammer <75260070+ANormalProgrammer@users.noreply.github.com>
2023-04-02 21:39:33 +0700

    fix code to comply with C++ standard practice.

--- snto.cpp
+++ snto.cpp
@@ -1,20 +1,28 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long
-bool check(ll n) // dpt O( sqrt(n)/6 )
+#include <iostream>
+// We shouldn't use <bits/stdc.h> and using namespace std;
+// https://stackoverflow.com/questions/31816095/why-should-i-not-include-bits-stdc-h
+// https://stackoverflow.com/questions/1452721/why-is-using-namespace-std-considered-bad-practice
+bool is_prime(long long number) // more descriptve name
 {
-    if(n<4) return true;
-    if(n%2==0 || n%3==0) return false;
-    for(int i=5;i<=sqrt(n);i+=6)
-        if(n%i==0 || n%(i+2)==0) return false;
+    // this will give correct result when number == 1
+    if(number < 1) return false;
+    if(number == 2 || number == 3) return false;
+
+    if(n % 2 == 0 || n % 3 == 0) return false;
+    for(int i = 5; i * i <= n; i += 6) // sqrt(n) takes lots of tie to compute
+        if(number % i==0 || number%(i+2)==0) 
+            return false;
     return true;
 }
 int main()
 {
-    ios_base::sync_with_stdio(0);cout.tie(0);
-    ll n;
-    cin>>n;
-    n++;
-    while(!check(n)) n++;
-    cout<<n;
+    long long input;
+    std::cin >> input;
+    // use a do-while loop to avoid a input++; line
+    do
+    {
+        ++input;
+    }
+    while (!is_prime(input));
+    std::cout << input;
 }
